4ec0321d36e7e30b23c005d2271c6bc478c1ccae
mostafa <javanmehri@yahoo.com>
2016-07-03 18:56:10 -0700

    Graphic - Img class

---
+++ My projects/MyChess2/src/mos/Images.java
@@ -0,0 +1,14 @@
+package mos;
+
+import javafx.scene.image.Image;
+
+/**
+ * Created by MOS on 2016-07-03.
+ */
+public class Images
+{
+
+    public static final Image black_Rook = new Image("file:./img/black_Rook.png");
+    public static final Image white_Rook = new Image("file:./img/white_Rook.png");
+
+}
--- My projects/MyChess2/src/mos/MainStageController.java
+++ My projects/MyChess2/src/mos/MainStageController.java
@@ -1,22 +1,23 @@
 package mos;
 
 import javafx.fxml.FXML;
-import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
 public class MainStageController {
 
 
-    Image black_Rook = new Image("file:Black_Rook.png");
+
 
     @FXML
-    private ImageView A1 = new ImageView(black_Rook);
+    private ImageView A1 = new ImageView();
 
     @FXML
     private void move()
     {
-        //A1.setImage(null);
+
+        A1.setImage(Images.white_Rook);
         //A1.setLayoutX(5);
+
     }
 
 }
